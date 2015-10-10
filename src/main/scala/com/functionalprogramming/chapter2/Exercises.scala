854137e4f1a6e2168b99ebee11d8e16d1209f59f
Thiago Nunes <thiagotnunes@gmail.com>
2015-10-10 21:16:21 +0200

    Adds execises 2.3, 2.4 and 2.5

--- src/main/scala/com/functionalprogramming/chapter2/Exercises.scala
+++ src/main/scala/com/functionalprogramming/chapter2/Exercises.scala
@@ -17,4 +17,16 @@ class Exercises {
       case _ => fib(n - 2) + fib(n - 1)
     }
   }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  }
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
 }
